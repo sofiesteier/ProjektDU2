caac943ce682f8f2bb97840a24991b5deccbf071
sofiesteier <steiersofie@gmail.com>
2023-01-06 13:29:07 +0100

    Update index.js

--- problem-2/index.js
+++ problem-2/index.js
@@ -15,9 +15,7 @@
 
 
 // Create Filter Elements
-create_levels_filter();
-create_subjects_filter();
-create_language_filter();
+
 create_countries_cities_filters();
 
 // Add Interaction of search field button
